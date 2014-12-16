428eccda07dd120d5668f86f2660be845f209b7b
m-mazur <mazur.marcus@gmail.com>
2014-12-17 00:56:05 +0100

    DB settings

--- ScoreKeeper/Repositories/ScoreRepository.cs
+++ ScoreKeeper/Repositories/ScoreRepository.cs
@@ -7,13 +7,14 @@ using System.Data;
 using System.Data.SqlClient;
 using System.Configuration;
 using ScoreKeeper.Models;
+using System.Data.Entity;
 
 namespace ScoreKeeper.Repositories
 {
     public class ScoreRepository : IScoreRepository
     {
-        private IDbConnection db = new SqlConnection(ConfigurationManager.ConnectionStrings["ScoreKeeperRelease"].ConnectionString);
-
+        private IDbConnection db = new SqlConnection(ConfigurationManager.ConnectionStrings["strykDB"].ConnectionString);
+    
         public IEnumerable<Score> GetAllScores()
         {
             return this.db.Query<Score>("SELECT * FROM Scores INNER JOIN Users ON Scores.UserId = Users.UserId");
