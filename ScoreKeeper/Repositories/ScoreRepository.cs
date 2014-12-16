23598264737597f745180d454b438b00c401b6a7
m-mazur <mazur.marcus@gmail.com>
2014-12-17 00:57:22 +0100

    DB settings

--- ScoreKeeper/Repositories/ScoreRepository.cs
+++ ScoreKeeper/Repositories/ScoreRepository.cs
@@ -13,7 +13,7 @@ namespace ScoreKeeper.Repositories
 {
     public class ScoreRepository : IScoreRepository
     {
-        private IDbConnection db = new SqlConnection(ConfigurationManager.ConnectionStrings["strykDB"].ConnectionString);
+        private IDbConnection db = new SqlConnection(ConfigurationManager.ConnectionStrings["ScoreKeeperRelease"].ConnectionString);
     
         public IEnumerable<Score> GetAllScores()
         {
