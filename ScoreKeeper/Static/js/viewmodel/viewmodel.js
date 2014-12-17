a74639c86e6da711e54db287f0c24a65a1464c9e
Marcus Mazur <marcus.mazur@tretton37.com>
2014-12-17 22:00:00 +0100

    Testing get request

--- ScoreKeeper/Static/js/viewmodel/viewmodel.js
+++ ScoreKeeper/Static/js/viewmodel/viewmodel.js
@@ -3,7 +3,7 @@
         var self = this;
         self.hello = ko.observable("hello");
 
-        self.scores = ko.observableArray(scoreService.getAllScores);
+        self.scores = ko.observableArray(scoreService.getAllScores());
         console.log(self.scores());
     };
     ko.applyBindings(ViewModel(ScoresService.get()));
