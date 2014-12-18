3bb0e1171fe3c80360bb1a272e8d6abf6767552d
m-mazur <mazur.marcus@gmail.com>
2014-12-18 22:38:13 +0100

    Added Repository and Controller for Users

---
+++ ScoreKeeper/Controllers/UsersController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using ScoreKeeper.Models;
+using ScoreKeeper.Repositories;
+
+namespace ScoreKeeper.Controllers
+{
+    public class UsersController : ApiController
+    {
+        private UsersRepository userRepository = new UsersRepository();
+
+        //GET api/Users
+        public IEnumerable<User> GetUsers()
+        {
+            return userRepository.GetAllUsers();
+        }
+    }
+}
---
+++ ScoreKeeper/Repositories/UsersRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ScoreKeeper.Models;
+using System.Data.SqlClient;
+using System.Data;
+using Dapper;
+using System.Configuration;
+
+namespace ScoreKeeper.Repositories
+{
+    public class UsersRepository : IUserRepository
+    {
+        private IDbConnection db = new SqlConnection(ConfigurationManager.ConnectionStrings["ScoreKeeperRelease"].ConnectionString);
+
+        public IEnumerable<User> GetAllUsers()
+        {
+            var sqlCmd = "SELECT * FROM Users";
+            return this.db.Query<User>(sqlCmd);
+        }
+    }
+}
