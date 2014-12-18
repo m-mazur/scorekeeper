7d9bfb9b28dd5c513427fe2e1208adcd4d85ae8f
m-mazur <mazur.marcus@gmail.com>
2014-12-18 20:12:56 +0100

    Initial work on viewmodel

---
+++ ScoreKeeper/Static/js/viewmodel.js
@@ -0,0 +1,14 @@
+﻿(function () {
+    var uri = "/api/Scores/";
+
+    var viewModel = {
+        scores: ko.observableArray()
+    };
+
+    $.getJSON(uri)
+    .done(function (data) {
+        viewModel.scores(data);
+    });
+
+    ko.applyBindings(viewModel);
+})();
