645e31b37c356c8419e9c12da8dca25bbb121e53
m-mazur <mazur.marcus@gmail.com>
2014-12-19 21:33:20 +0100

    Implemented leaderboard function

--- ScoreKeeper/Static/js/viewmodel.js
+++ ScoreKeeper/Static/js/viewmodel.js
@@ -5,8 +5,24 @@
     var viewModel = {
         scores: ko.observableArray(),
         users: ko.observableArray(),
+        leaderboard: ko.observableArray(),
+        scoresOptions: ko.observableArray([
+            { option: 1 },
+            { option: 2 },
+            { option: 3 },
+            { option: 4 },
+            { option: 5 },
+            { option: 6 },
+            { option: 7 },
+            { option: 8 },
+            { option: 9 },
+            { option: 10 },
+            { option: 11 },
+            { option: 12 },
+            { option: 13 }
+        ]),
         newScore: {
-            ScorePoints: ko.observable(),
+            ScorePoints: ko.observable("1"),
             UserId: ko.observable()
         }
     };
@@ -22,9 +38,29 @@
         });
     };
 
+    function createLeaderboard() {
+        var groupedScores = _.groupBy(viewModel.scores(), function (score) {
+            return score.UserName;
+        });
+
+        var mappedScores = _.chain(groupedScores).map(function (board, key) {
+            return {
+                UserName: key,
+                ScorePoints: _.reduce((_.pluck(board, "ScorePoints")), function (result, current) {
+                    return result + current;
+                })
+            }
+        }).value();
+
+        return (_.sortBy(mappedScores, function(list) {
+            return list.ScorePoints;
+        })).reverse();
+    };
+
     function getAllScores () {
         ajaxHelper(scoresUri, 'GET').done(function (data) {
             viewModel.scores(data);
+            viewModel.leaderboard(createLeaderboard());
         });
     }
 
--- ScoreKeeper/bin/Static/js/viewmodel.js
+++ ScoreKeeper/bin/Static/js/viewmodel.js
@@ -5,8 +5,24 @@
     var viewModel = {
         scores: ko.observableArray(),
         users: ko.observableArray(),
+        leaderBoard: ko.observableArray(),
+        scoresOptions: ko.observableArray([
+            { option: 1 },
+            { option: 2 },
+            { option: 3 },
+            { option: 4 },
+            { option: 5 },
+            { option: 6 },
+            { option: 7 },
+            { option: 8 },
+            { option: 9 },
+            { option: 10 },
+            { option: 11 },
+            { option: 12 },
+            { option: 13 }
+        ]),
         newScore: {
-            ScorePoints: ko.observable(),
+            ScorePoints: ko.observable("1"),
             UserId: ko.observable()
         }
     };
@@ -22,9 +38,17 @@
         });
     };
 
+   function leaderBoard (){
+        var board = _.groupBy(viewModel.scores(), function(score) {
+            return score.UserId;
+        });
+       console.log(board);
+    };
+
     function getAllScores () {
         ajaxHelper(scoresUri, 'GET').done(function (data) {
             viewModel.scores(data);
+            leaderBoard();
         });
     }
 
