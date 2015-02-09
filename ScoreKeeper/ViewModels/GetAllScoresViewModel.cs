4be7edb99c13328915c01c96b7389a2da8010e7c
m-mazur <mazur.marcus@gmail.com>
2015-02-09 22:50:01 +0100

    Refactoring

---
+++ ScoreKeeper/ViewModels/GetAllScoresViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ScoreKeeper.Repositories;
+using ScoreKeeper.Models;
+using AutoMapper;
+
+namespace ScoreKeeper.ViewModels
+{
+    public class GetAllScoresViewModel
+    {
+        private ScoreRepository scoreRepository;
+
+        public GetAllScoresViewModel()
+        {
+            scoreRepository = new ScoreRepository();
+        }
+       
+        public IEnumerable<GetAllScoresViewModel> GetAllScores()
+        {
+            Mapper.CreateMap<Score, GetAllScoresViewModel>();
+            return Mapper.Map<IEnumerable<GetAllScoresViewModel>>(scoreRepository.GetAllScores());
+        }
+
+        public string UserName { get; set; }
+        public int ScorePoints { get; set; }
+        public DateTime ScoreDate { get; set; }
+    }
+}
