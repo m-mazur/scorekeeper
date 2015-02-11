7f7124860273def7e2ad9837393beb8ac6e3fbc4
m-mazur <mazur.marcus@gmail.com>
2015-02-11 13:23:57 +0100

    Added seperate View Models for leaderboard and results

---
+++ ScoreKeeper/Static/js/app.new-viewmodel.js
@@ -0,0 +1,22 @@
+﻿(function () {
+    var ViewModel = function (resultsViewModel, leaderBoardViewModel) {
+        var self = this;
+
+        self.scores = ko.observableArray();
+        self.leaderboard = ko.observableArray();
+
+        resultsViewModel.getAllScores().done(function (data) {
+            self.scores(data);
+        }).then(function (data){
+            self.leaderboard(leaderBoardViewModel.createLeaderboard(data));
+        });;
+
+        self.deleteScore = function (score) {
+            resultsViewModel.deleteScore(score).done(function () {
+                self.scores.remove(score);
+            });
+        }
+    }
+
+    ko.applyBindings(ViewModel(new ResultsViewModel(), new LeaderBoardViewModel()));
+}());
